379aec6020f0e53a397ee92101dc64358f2f88a0
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-12-13 12:30:33 +0530

    Update proxy1.js

--- proxy1.js
+++ proxy1.js
@@ -1,24 +1,24 @@
 "use strict";
-import UserAgent from 'user-agents';
+
 import sharp from "sharp";
 import pick from "./pick.js";
 import superagent from "superagent";
 import { availableParallelism } from "os";
-
+import UserAgent from 'user-agents';const userAgent = new UserAgent();
 // Constants
 const DEFAULT_QUALITY = 40;
 const MIN_COMPRESS_LENGTH = 1024;
 const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100;
 const MAX_HEIGHT = 16383;
-const USER_AGENT = "Bandwidth-Hero Compressor";
+
 
 /**
  * Copies headers from the source response to the target response, logging errors if any.
  * @param {Object} sourceHeaders - The headers from the source response.
  * @param {http.ServerResponse} target - The target response object.
  */
-function copyHeaders(source, target) {
-  Object.entries(source.headers).forEach(([key, value]) => {
+function copyHeaders(sourceHeaders, target) {
+  Object.entries(sourceHeaders).forEach(([key, value]) => {
     try {
       target.setHeader(key, value);
     } catch (e) {
@@ -60,78 +60,60 @@ function redirect(req, res) {
   res.statusCode = 302;
   res.end();
 }
+
 /**
  * Compresses and transforms the image according to request parameters.
  * @param {http.IncomingMessage} req - The incoming HTTP request.
  * @param {http.ServerResponse} res - The HTTP response.
  * @param {stream.Readable} input - The input image stream.
  */
-
-//const sharpStream = _ => sharp({ animated: !process.env.NO_ANIMATE, unlimited: true });
-const sharpStream = _ => sharp({ animated: false, unlimited: true });
-
 function compress(req, res, input) {
-  const format = req.params.webp ? 'webp' : 'jpeg';
+  const format = req.params.webp ? "webp" : "jpeg";
+  const sharpInstance = sharp(input, {
+    unlimited: true,
+    failOn: "none",
+    limitInputPixels: false,
+  });
 
-  /*
-   * Determine the uncompressed image size when there's no content-length header. Only do metadata thing and do not change other things.
-   */
+  sharp.cache(false);
+  sharp.simd(false);
+  sharp.concurrency(availableParallelism());
 
-  /*
-   * input.pipe => sharp (The compressor) => Send to httpResponse
-   * The following headers:
-   * |  Header Name  |            Description            |           Value            |
-   * |---------------|-----------------------------------|----------------------------|
-   * |x-original-size|Original photo size                |OriginSize                  |
-   * |x-bytes-saved  |Saved bandwidth from original photo|OriginSize - Compressed Size|
-   */
-
-  input.pipe(sharpStream()
-    .metadata((err, metadata) => {
-      if (err) return redirect(req, res);
-
-      let transform = sharpStream();
-
-      if (metadata.height > 16383) {
-        transform = transform.resize(null, 16383, { withoutEnlargement: true });
+  sharpInstance
+    .metadata()
+    .then((metadata) => {
+      if (metadata.height > MAX_HEIGHT) {
+        sharpInstance.resize({
+          width: null,
+          height: MAX_HEIGHT,
+          withoutEnlargement: true,
+        });
       }
-
-      transform
+      return sharpInstance
         .grayscale(req.params.grayscale)
-        .toFormat(format, {
-          quality: req.params.quality,
-          progressive: true,
-          optimizeScans: true
-        })
-        .toBuffer((err, output, info) => _sendResponse(err, output, info, format, req, res));
+        .toFormat(format, { quality: req.params.quality, effort: 0 })
+        .toBuffer();
     })
-  );
+    .then((outputBuffer) => {
+      res.writeHead(200, {
+        "content-type": `image/${format}`,
+        "content-length": outputBuffer.length,
+        "x-original-size": req.params.originSize,
+        "x-bytes-saved": req.params.originSize - outputBuffer.length,
+      });
+      res.end(outputBuffer);
+    })
+    .catch(() => redirect(req, res));
 }
-
-function _sendResponse(err, output, info, format, req, res) {
-  if (err || !info) return redirect(req, res);
-
-  res.setHeader('content-type', 'image/' + format);
-  res.setHeader('content-length', info.size);
-  res.setHeader('x-original-size', req.params.originSize);
-  res.setHeader('x-bytes-saved', req.params.originSize - info.size);
-  res.status(200);
-  res.write(output);
-  res.end();
-}
-
-
-
 
 /**
  * Main proxy handler for bandwidth optimization.
  * @param {http.IncomingMessage} req - The incoming HTTP request.
  * @param {http.ServerResponse} res - The HTTP response.
  */
-function hhproxy(req, res) {
+async function hhproxy(req, res) {
   let url = req.query.url;
   if (!url) return res.end("bandwidth-hero-proxy");
-  const userAgent = new UserAgent();
 
   req.params = {
     url: decodeURIComponent(url),
@@ -147,41 +129,40 @@ function hhproxy(req, res) {
     return redirect(req, res);
   }
 
-  superagent
-    .get(req.params.url)
-    .set({
-      ...pick(req.headers, ["cookie", "dnt", "referer", "range"]),
-      "User-Agent": userAgent.toString(),
-      "X-Forwarded-For": req.headers["x-forwarded-for"] || req.ip,
-      "Via": "1.1 bandwidth-hero",
-    })
-    .responseType("stream") // Set response type to stream
-    .then((response) => {
-      req.params.originType = response.headers["content-type"] || "";
-      req.params.originSize = parseInt(response.headers["content-length"] || "0");
+  try {
+    const response = await superagent
+      .get(req.params.url)
+      .set({
+        ...pick(req.headers, ["cookie", "dnt", "referer", "range"]),
+        "User-Agent": userAgent.toString(),
+        "X-Forwarded-For": req.headers["x-forwarded-for"] || req.ip,
+        "Via": "1.1 bandwidth-hero",
+      })
+      .responseType("stream"); // Set response type to stream
 
-      if (shouldCompress(req)) {
-        compress(req, res, response.body);
-      } else {
-        res.writeHead(200, {
-          "Access-Control-Allow-Origin": "*",
-          "Cross-Origin-Resource-Policy": "cross-origin",
-          "Cross-Origin-Embedder-Policy": "unsafe-none",
-          "X-Proxy-Bypass": 1,
-        });
-        copyHeaders(response, res); // Use copyHeaders here
-        response.body.pipe(res); // Stream original response to the client
-      }
-    })
-    .catch((err) => {
-      if (err.status === 404 || err.response?.headers?.location) {
-        redirect(req, res);
-      } else {
-        res.status(400).send("Invalid URL");
-        console.error(err);
-      }
-    });
+    req.params.originType = response.headers["content-type"] || "";
+    req.params.originSize = parseInt(response.headers["content-length"] || "0");
+
+    if (shouldCompress(req)) {
+      compress(req, res, response.body);
+    } else {
+      res.writeHead(200, {
+        "Access-Control-Allow-Origin": "*",
+        "Cross-Origin-Resource-Policy": "cross-origin",
+        "Cross-Origin-Embedder-Policy": "unsafe-none",
+        "X-Proxy-Bypass": 1,
+      });
+      copyHeaders(response.headers, res); // Use copyHeaders here
+      response.body.pipe(res); // Stream original response to the client
+    }
+  } catch (err) {
+    if (err.status === 404 || err.response?.headers?.location) {
+      redirect(req, res);
+    } else {
+      res.status(400).send("Invalid URL");
+      console.error(err);
+    }
+  }
 }
-
 
 export default hhproxy;
