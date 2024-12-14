b1d63abaeef097cbc22879c83aca1be46f2776c5
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-12-14 15:52:35 +0530

    Update proxy1.js

--- proxy1.js
+++ proxy1.js
@@ -1,4 +1,21 @@
 "use strict";
+import { execFile } from 'node:child_process';
+import gif2webp from 'gif2webp-bin';
+import isAnimated from 'is-animated';
+import fs from  'fs';
+import os from 'os';
+import { URL } from 'url';
+import cacheMgr from 'cache-manager';
+import cacheStore from'cache-manager-fs-binary';
+const cache = cacheMgr.caching({
+    store: cacheStore,
+    options: {
+        ttl: 604800, //7d
+        maxsize: 1073741824, //1GB
+        path: './cache',
+        preventfill: true
+    }
+})
 
 import request from 'superagent';
 import sharp from "sharp";
@@ -48,38 +65,103 @@ function redirect(req, res) {
 }
 
 // Helper: Compress
-
-//const sharpStream = _ => sharp({ animated: !process.env.NO_ANIMATE, unlimited: true });
 function compress(req, res, input) {
-  const format = req.params.webp ? 'webp' : 'jpeg'
-  const compressionQuality = req.params.quality * 0.1
-  
-  req.params.quality = Math.ceil(compressionQuality)
-  
-  return sharp(input)
-		.resize({
-			width: null,
-			height: 16383
+    const format = 'webp'
+    const originType = req.params.originType
+    const key = new URL(req.params.url) || ''
+    const stats = sharp.cache();
+    sharp.cache( { memory: 200 } );
+    const threads = sharp.concurrency(0)
+
+    if (originType.endsWith('gif') && isAnimated(input)) {
+        let { hostname, pathname } = new URL(req.params.url)
+
+        let path = `${os.tmpdir()}/${hostname + encodeURIComponent(pathname)}`;
+        fs.writeFile(path + '.gif', input, (err) => {
+            console.error(err)
+            if (err) return redirect(req, res)
+            //defer to gif2webp *higher latency*
+            execFile(gif2webp, ['-mixed', '-m', 6, '-q', 50, '-mt', '-v', `${path}.gif`, '-o', `${path}.webp`],
+                    (convErr) => {
+                    if (convErr) console.error(convErr)
+                    console.log('GIF Image converted!')
+                    fs.readFile(`${path}.webp`, (readErr, data) => {
+                        if(readErr) console.error(readErr);
+                        if (readErr || res.headersSent) return redirect(req, res)
+                        setResponseHeaders(fs.statSync(`${path}.webp`), 'webp')
+
+                        //initiate cleanup procedures
+                        fs.unlink(`${path}.gif`, function () { })
+                        fs.unlink(`${path}.webp`, function () { })
+
+			res.write(data)
+                        res.end()
+						})
+					}
+				)
 		})
-		.grayscale(req.params.grayscale)
-		.toFormat(format, {
-			quality: req.params.quality,
-			preset: 'picture',
-			effort: 0
-		})
-		.toBuffer({resolveWithObject: true})
-		.then(({data: output, info}) => {	// this way we can also get the info about output image, like height, width
-		  res.setHeader('content-type', 'image/' + format);
-                  res.setHeader('content-length', info.size);
-                  res.setHeader('x-original-size', req.params.originSize);                  res.setHeader('x-bytes-saved', req.params.originSize - info.size);
-                  res.status(200);
-                  res.write(output);
-                  res.end();
-		}).catch(err => {
-			return {
-				err: err
-			};
-		});
+
+    }else {
+
+        const image = sharp(input);
+
+        image
+            .metadata(function (err, metadata) {
+                let pixelCount = metadata.width * metadata.height;
+                var compressionQuality = req.params.quality;
+
+                //3MP or 1.5MB
+                if (pixelCount > 3000000 || metadata.size > 1536000) {
+                    compressionQuality *= 0.1
+                    //2MP or 1MB
+                } else if (pixelCount > 2000000 && metadata.size > 1024000) {
+                    compressionQuality *= 0.25
+                    //1MP or 512KB
+                } else if (pixelCount > 1000000 && metadata.size > 512000) {
+                    compressionQuality *= 0.5
+                    //0.5MP or 256KB
+                } else if (pixelCount > 500000 && metadata.size > 256000) {
+                    compressionQuality *= 0.75
+                }
+                compressionQuality = Math.ceil(compressionQuality)
+
+                cache.wrap(key, (callback) => {
+                    sharp(input)
+                        .grayscale(req.params.grayscale)
+                        .toFormat(format, {
+                            quality: compressionQuality,
+                            progressive: false,
+                            optimizeScans: false,
+                            effort: 1,
+                            smartSubsample: true,
+                            lossless: false
+
+                        })
+                        .toBuffer((err, output, info) => {
+                            callback(err, { binary: { output: output }, info: info })
+                        })
+                        
+                }, (err, obj) => {
+                    if (err || !obj || !obj.info || res.headersSent) return redirect(req, res)
+                 setResponseHeaders(obj.info, format)
+                            res.status(200)
+                            res.write(obj.binary.output)
+                            res.end()
+
+
+                    
+                })
+            })
+    }
+    function setResponseHeaders(info, imgFormat) {
+        res.setHeader('content-type', `image/${imgFormat}`)
+        res.setHeader('content-length', info.size)
+        let filename = (new URL(req.params.url).pathname.split('/').pop() || "image") + '.' + format
+        res.setHeader('Content-Disposition', 'inline; filename="' + filename + '"')
+        res.setHeader('x-original-size', req.params.originSize)
+        res.setHeader('x-bytes-saved', req.params.originSize - info.size)
+    }
+
 }
 
 
