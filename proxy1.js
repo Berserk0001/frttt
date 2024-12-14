9089364474e9fc54e6e3ac175c673b64e77e6242
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-12-14 16:03:16 +0530

    Update proxy1.js

--- proxy1.js
+++ proxy1.js
@@ -1,13 +1,9 @@
 "use strict";
-import { execFile } from 'node:child_process';
-import gif2webp from 'gif2webp-bin';
-import isAnimated from 'is-animated';
-import fs from  'fs';
-import os from 'os';
+
 import { URL } from 'url';
 import cacheMgr from 'cache-manager';
 import cacheStore from'cache-manager-fs-binary';
-const cache = cacheMgr.caching({
+import cache from cacheMgr.caching({
     store: cacheStore,
     options: {
         ttl: 604800, //7d
@@ -16,7 +12,6 @@ const cache = cacheMgr.caching({
         preventfill: true
     }
 })
-
 import request from 'superagent';
 import sharp from "sharp";
 import pick from "./pick.js";
@@ -67,41 +62,11 @@ function redirect(req, res) {
 // Helper: Compress
 function compress(req, res, input) {
     const format = 'webp'
-    const originType = req.params.originType
     const key = new URL(req.params.url) || ''
     const stats = sharp.cache();
     sharp.cache( { memory: 200 } );
     const threads = sharp.concurrency(0)
 
-    if (originType.endsWith('gif') && isAnimated(input)) {
-        let { hostname, pathname } = new URL(req.params.url)
-
-        let path = `${os.tmpdir()}/${hostname + encodeURIComponent(pathname)}`;
-        fs.writeFile(path + '.gif', input, (err) => {
-            console.error(err)
-            if (err) return redirect(req, res)
-            //defer to gif2webp *higher latency*
-            execFile(gif2webp, ['-mixed', '-m', 6, '-q', 50, '-mt', '-v', `${path}.gif`, '-o', `${path}.webp`],
-                    (convErr) => {
-                    if (convErr) console.error(convErr)
-                    console.log('GIF Image converted!')
-                    fs.readFile(`${path}.webp`, (readErr, data) => {
-                        if(readErr) console.error(readErr);
-                        if (readErr || res.headersSent) return redirect(req, res)
-                        setResponseHeaders(fs.statSync(`${path}.webp`), 'webp')
-
-                        //initiate cleanup procedures
-                        fs.unlink(`${path}.gif`, function () { })
-                        fs.unlink(`${path}.webp`, function () { })
-
-			res.write(data)
-                        res.end()
-						})
-					}
-				)
-		})
-
-    }else {
 
         const image = sharp(input);
 
@@ -152,7 +117,7 @@ function compress(req, res, input) {
                     
                 })
             })
-    }
+    
     function setResponseHeaders(info, imgFormat) {
         res.setHeader('content-type', `image/${imgFormat}`)
         res.setHeader('content-length', info.size)
