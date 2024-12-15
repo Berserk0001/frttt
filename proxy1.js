18f5ecd0086b598055e9f83e05c887689752d5e2
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2024-12-15 10:01:43 +0530

    Update proxy1.js

--- proxy1.js
+++ proxy1.js
@@ -69,7 +69,7 @@ function compress(req, res, input) {
             resizeHeight = 16383;
         }
 
-        image
+        sharp(input)
             .resize({
                 width: resizeWidth,
                 height: resizeHeight
